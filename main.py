7171667f0be5762e52e98130c4dd24bd3a6842d7
Mick214 <uaeauh21@gmail.com>
2022-11-22 21:19:05 +0400

    Debug

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI
-from router import blog_get
-from router import blog_post
-from router import user
-from router import article
-from router import product
+from router import blog_get, blog_post, user, article, product, file
 from auth import authentication
 from db import models
 from db.database import engine
@@ -12,10 +8,12 @@ from fastapi.responses import JSONResponse
 from fastapi.exceptions import HTTPException
 from fastapi.responses import PlainTextResponse
 from exceptions import StoryException
-from fastapi.middleware.cors import CORSMiddleware    
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles 
 
 app = FastAPI()
 app.include_router(authentication.router)
+app.include_router(file.router)
 app.include_router(user.router)
 app.include_router(article.router)
 app.include_router(product.router)
@@ -53,3 +51,6 @@ app.add_middleware(
   allow_methods = ["*"],
   allow_headers= ["*"]
 )
+
+
+app.mount('/files', StaticFiles(directory="files"), name='files')
--- router/article.py
+++ router/article.py
@@ -1,4 +1,4 @@
-from schemas import ArticleBase, ArticleDisplay, UserBase
+from schemas import ArticleBase, ArticleDisplay, UserBase, UserDisplay
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from db.database import get_db
@@ -17,7 +17,7 @@ def create_article(request: ArticleBase, db: Session = Depends(get_db), current_
   return db_article.create_article(db, request)
 
 # Get specific article
-@router.get('/{id}') #, response_model=ArticleDisplay)
+@router.get('/{id}')#, response_model=ArticleDisplay)
 def get_article(id: int, db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
   return {
    'data': db_article.get_article(db, id),
